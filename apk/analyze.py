315fea9e8cf7b58e5a150130e04f51a889bc8959
Floor Terra <floort@gmail.com>
2014-02-14 22:59:22 +0100

    Fixed decompilaton bug

--- apk/analyze.py
+++ apk/analyze.py
@@ -16,16 +16,22 @@ from androguard.core.bytecodes.apk import *
 from androguard.core.analysis.analysis import *
 from androguard.core.analysis.ganalysis import *
 from androguard.core.analysis.risk import *
-from androguard.decompiler.dad import decompile
+from androguard.decompiler.decompiler import *
 
 def get_classes(apk_filename):
-    a = apk.APK(apk_filename)
-    d = dvm.DalvikVMFormat(a.get_dex())
-    vmx = analysis.VMAnalysis(d)
-
-    for cur_class in d.get_classes():
-        dc = decompile.DvClass(cur_class, vmx)
-        yield [cur_class.get_name(), dc.get_source()]
+	a = apk.APK(apk_filename)
+	d = dvm.DalvikVMFormat(a.get_dex())
+	d.create_python_export()
+	dx = uVMAnalysis(d)
+	gx = GVMAnalysis(dx, None)
+	d.set_vmanalysis(dx)
+	d.set_gvmanalysis(gx)
+	d.set_decompiler(DecompilerDAD(d, dx))
+	d.create_xref()
+	d.create_dref()
+	
+	for cur_class in d.get_classes():
+		yield [cur_class.get_name(), cur_class.get_source()]
 
 def get_all_classes(filename):
     a = apk.APK(filename)
